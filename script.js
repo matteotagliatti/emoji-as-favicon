fa020582354202e2e7be5d1f1dae2d1d50b48635
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-01-10 08:41:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const selectFavicon = document.querySelector(`head > link[rel='icon']`);
+const buttons = document.querySelectorAll(`.grid > button`);
+
+function generateFavicon(string, icon) {
+  return `
+      <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+        <text y=%22.9em%22 font-size=%2290%22>
+          ${icon}
+        </text>
+      </svg>
+    `.trim();
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    newFavicon = generateFavicon`${button.innerText}`;
+    selectFavicon.setAttribute(`href`, `data:image/svg+xml,${newFavicon}`);
+  });
+});
